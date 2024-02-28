11260aa91f338ea88f32cc2a62a739805bbe5353
Akila Fernando <144292336+Akila-Fernando@users.noreply.github.com>
2024-02-28 12:57:15 -0700

    Added code, need to test

--- ex4.2.py
+++ ex4.2.py
@@ -1,9 +1,33 @@
+import timeit
+
+
 # Efficient Code
 
-def efficientsort():
+def efficientsort(arr, search_term, low, high):
+    while low <= high:
+        mid = low +(high-low)//2
+
+        if arr[mid] == search_term:
+            return True
+        elif arr[mid] < search_term:
+            low = mid+1
+        elif arr[mid] > search_term:
+            high = mid-1
 
 
 
 # Ineffeicient Code
     
-def inefficientsort():
+def inefficientsort(arr, search_term):
+    n = len(arr)
+    for i in range(n):
+        if arr[i] == search_term:
+            return True
+
+
+Sortedarray = [1,2,3,4,5,6,7,8,9]
+
+search = 8
+
+efficientsortresult = efficientsort(Sortedarray, search,0,len(Sortedarray)-1)
+inefficientsortresult = inefficientsort(Sortedarray, search)
